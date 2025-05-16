ba3da5721518c1374933c9ca49313e25f3e8b7b6
Mjawadhachem <mjawadhachem@gmail.com>
2025-05-16 17:24:22 +0300

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  // Hero animations
+  document.querySelector(".hero-image").style.cssText =
+    "opacity:1; transform:translateX(0);";
+  document.querySelector(".hero-content").style.cssText =
+    "opacity:1; transform:translateY(0);";
+  document.querySelector(".watermark").style.opacity = "1";
+});
